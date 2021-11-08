1e06324b870544c106e330223793dfb55ab25ee5
teodor201 <35153619+teodor201@users.noreply.github.com>
2021-11-08 11:29:13 +0200

    Added fields to user and car entity

--- src/main/java/com/park/parkinglot/entity/Car.java
+++ src/main/java/com/park/parkinglot/entity/Car.java
@@ -7,9 +7,12 @@ package com.park.parkinglot.entity;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 /**
@@ -24,6 +27,38 @@ public class Car implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private String licensePlate;
+    
+    private String parkingSpot;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_KEY")
+    private User user;
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getParkingSpot() {
+        return parkingSpot;
+    }
+
+    public void setParkingSpot(String parkingSpot) {
+        this.parkingSpot = parkingSpot;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public Integer getId() {
         return id;
@@ -57,5 +92,5 @@ public class Car implements Serializable {
     public String toString() {
         return "com.park.parkinglot.entity.Car[ id=" + id + " ]";
     }
-    
+
 }
--- src/main/java/com/park/parkinglot/entity/User.java
+++ src/main/java/com/park/parkinglot/entity/User.java
@@ -6,10 +6,12 @@
 package com.park.parkinglot.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -25,6 +27,57 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    private String username;
+
+    private String email;
+    
+    private String password;
+    
+    private String position;
+    
+    @OneToMany(mappedBy= "user")
+    private Collection<Car> cars;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Collection<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(Collection<Car> cars) {
+        this.cars = cars;
+    }
 
     public Integer getId() {
         return id;
@@ -58,5 +111,5 @@ public class User implements Serializable {
     public String toString() {
         return "com.park.parkinglot.entity.User[ id=" + id + " ]";
     }
-    
+
 }
